0013f5fda8e8e6b3ab66e775a223003df75cdbff
zhargal <bzhargal@gmail.com>
2023-01-02 10:43:28 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int spaceCount = cathetusLength - 1;
+        int symbolCount = 1;
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < spaceCount; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < symbolCount; j++) {
+                System.out.print("*");
+            }
+            spaceCount--;
+            symbolCount++;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,32 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int bottom = cathetusLength * 2 - 1;
+        int spaceCount = bottom/2;
+        int symbolCount = 1;
+        int symbolValue = 1;
 
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < spaceCount; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= symbolCount/2; j++) {
+                System.out.print(symbolValue);
+                symbolValue--;
+            }
+            symbolValue++;
+            for (int j = symbolCount / 2 + 1; j < symbolCount; j++) {
+                ++symbolValue;
+                System.out.print(symbolValue);
+            }
+            spaceCount--;
+            symbolCount += 2;
+            symbolValue++;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(6);
     }
 }
